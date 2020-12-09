c55cbd319ed7097ad4779c3f22d5e565b9d40dce
Kamil Jędrychowski <kamil1998521@gmail.com>
2020-12-09 17:59:00 +0100

    Sending status history data to mongoDb

--- domain/domain-device-status/src/main/java/com/iot/device_status/stream/BaseDeviceEventSinkStream.java
+++ domain/domain-device-status/src/main/java/com/iot/device_status/stream/BaseDeviceEventSinkStream.java
@@ -5,7 +5,11 @@ import org.springframework.messaging.MessageChannel;
 
 public interface BaseDeviceEventSinkStream {
     String BASE_DEVICE_EVENTS_STREAM = "device-events-history-input-stream";
+    String DEVICE_STATUS_STREAM = "device-status-history-input-stream";
 
     @Input(BASE_DEVICE_EVENTS_STREAM)
     MessageChannel inputStream();
+
+    @Input(DEVICE_STATUS_STREAM)
+    MessageChannel inputStatusStream();
 }
